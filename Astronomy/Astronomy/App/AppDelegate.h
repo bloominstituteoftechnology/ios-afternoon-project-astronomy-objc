87c28caac31553f612e195a8d5196e87bfb90d9c
Christopher Aronson <christopheraronson@icloud.com>
2019-07-22 12:42:40 -0700

    Created Xcode project

---
+++ Astronomy/Astronomy/App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Astronomy
+//
+//  Created by Christopher Aronson on 7/22/19.
+//  Copyright © 2019 Christopher Aronson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Astronomy/Astronomy/View Controllers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Astronomy
+//
+//  Created by Christopher Aronson on 7/22/19.
+//  Copyright © 2019 Christopher Aronson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
