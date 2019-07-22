f40d76965a80818e129ebf496a9a52c64172bb34
Sameera Leola <pixelandbyte@gmail.com>
2019-07-22 17:46:24 -0500

    Initial commit

---
+++ Astronomy/Astronomy/Startup/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Astronomy
+//
+//  Created by Sameera Roussi on 7/22/19.
+//  Copyright © 2019 Sameera Roussi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
