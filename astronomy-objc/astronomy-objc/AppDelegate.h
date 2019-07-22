dd480a917c9d21090cc2999caa83e432d32261f8
hectorsvill <hectorstevenvillasano@gmail.com>
2019-07-22 12:44:26 -0700

    init

---
+++ astronomy-objc/astronomy-objc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  astronomy-objc
+//
+//  Created by Hector Steven on 7/22/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
