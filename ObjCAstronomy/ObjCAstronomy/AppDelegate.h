6388a35ee2c10bb84d1378e90818e7e65a846b11
Mitchell Budge <mitchellbudge@gmail.com>
2019-07-22 15:29:22 -0600

    Initial commit with new Xcode project

---
+++ ObjCAstronomy/ObjCAstronomy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjCAstronomy
+//
+//  Created by Mitchell Budge on 7/22/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
