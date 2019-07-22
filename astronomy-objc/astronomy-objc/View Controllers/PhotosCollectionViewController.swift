b56822bf70edfc8989be25ec021d7cb2f8b88482
hectorsvill <hectorstevenvillasano@gmail.com>
2019-07-22 15:04:19 -0700

    view Controllers

--- astronomy-objc/astronomy-objc/View Controllers/PhotosCollectionViewController.swift
+++ astronomy-objc/astronomy-objc/View Controllers/PhotosCollectionViewController.swift
@@ -6,4 +6,31 @@
 //  Copyright © 2019 Hector Steven. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+
+class PhotosCollectionViewController: UIViewController {
+	
+	@IBOutlet var collectionView: UICollectionView!
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		collectionView.dataSource = self
+		collectionView.delegate = self
+	}
+}
+
+extension PhotosCollectionViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		return 10
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PlanetCell", for: indexPath)
+		
+		
+		return cell
+	}
+	
+	
+}
