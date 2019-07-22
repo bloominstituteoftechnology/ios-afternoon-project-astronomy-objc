ed9f20b2199394638450953f59e59020c1789fc7
Thomas Cacciatore <tcacciatore597@gmail.com>
2019-07-22 17:22:07 -0400

    Setup Storyboard and UI

---
+++ AstronomyObjc/AstronomyObjc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AstronomyObjc
+//
+//  Created by Thomas Cacciatore on 7/22/19.
+//  Copyright © 2019 Thomas Cacciatore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AstronomyObjc/AstronomyObjc/ImageCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImageCollectionViewCell.swift
+//  AstronomyObjc
+//
+//  Created by Thomas Cacciatore on 7/22/19.
+//  Copyright © 2019 Thomas Cacciatore. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+}
