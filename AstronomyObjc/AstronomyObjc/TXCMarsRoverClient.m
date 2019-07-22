ddb9d1f844df02e12912b08e6fed981dc3a01f0c
Thomas Cacciatore <tcacciatore597@gmail.com>
2019-07-22 18:53:37 -0400

    added apiKey and base url to marsRoverClient

---
+++ AstronomyObjc/AstronomyObjc/TXCMarsRoverClient.m
@@ -0,0 +1,17 @@
+//
+//  TXCMarsRoverClient.m
+//  AstronomyObjc
+//
+//  Created by Thomas Cacciatore on 7/22/19.
+//  Copyright © 2019 Thomas Cacciatore. All rights reserved.
+//
+
+#import "TXCMarsRoverClient.h"
+
+@implementation TXCMarsRoverClient
+
+NSString *apiKey = @"5KgQqJgMjQ0odutTJgNvZbRjh86tV1lSXZBzg3Ri";
+static NSString *baseURL = @"https://api.nasa.gov/mars-photos/api/v1/";
+
+
+@end
