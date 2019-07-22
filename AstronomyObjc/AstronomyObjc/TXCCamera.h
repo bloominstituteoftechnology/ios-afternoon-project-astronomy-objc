0b0fec7a85c524006f9b54db9f9a3d09bab1f82f
Thomas Cacciatore <tcacciatore597@gmail.com>
2019-07-22 18:08:52 -0400

    Added MarsPhoto and Camera files

---
+++ AstronomyObjc/AstronomyObjc/TXCCamera.h
@@ -0,0 +1,22 @@
+//
+//  TXCCamera.h
+//  AstronomyObjc
+//
+//  Created by Thomas Cacciatore on 7/22/19.
+//  Copyright © 2019 Thomas Cacciatore. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+@interface TXCCamera : NSObject
+
+@property int identifier;
+@property NSString *name;
+@property int roverID;
+@property NSString *fullName;
+
+
+@end
+
