1090593363fe74a65dd73fa32761564e76310277
Christopher Aronson <christopheraronson@icloud.com>
2019-07-23 16:47:33 -0700

    Created UI and class for collection view cell

---
+++ Astronomy/Astronomy/Cell/PhotoCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Astronomy
+//
+//  Created by Christopher Aronson on 7/23/19.
+//  Copyright © 2019 Christopher Aronson. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var detailLabel: UILabel!
+}
