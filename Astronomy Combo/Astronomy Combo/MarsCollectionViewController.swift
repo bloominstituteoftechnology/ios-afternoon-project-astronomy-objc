78f530436451cdf02607cd5c221811561a7ded79
Michael Redig <mredig@gmail.com>
2019-10-14 15:48:02 -0500

    setup collection view controller

---
+++ Astronomy Combo/Astronomy Combo/MarsCollectionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MarsCollectionViewController.swift
+//  Astronomy Combo
+//
+//  Created by Michael Redig on 10/14/19.
+//  Copyright © 2019 Red_Egg Productions. All rights reserved.
+//
+
+import UIKit
+
+class MarsCollectionViewController: UICollectionViewController {
+
+
+}
+
