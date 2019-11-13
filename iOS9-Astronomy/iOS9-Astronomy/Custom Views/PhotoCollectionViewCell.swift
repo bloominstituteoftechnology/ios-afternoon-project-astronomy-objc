226977197310ff86d2fe74a9e665daffc3af239a
Jake <jlconnerly@gmail.com>
2019-11-12 20:40:17 -0600

    UI Setup complete

--- iOS9-Astronomy/iOS9-Astronomy/Custom Views/PhotoCollectionViewCell.swift
+++ iOS9-Astronomy/iOS9-Astronomy/Custom Views/PhotoCollectionViewCell.swift
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  PhotoCollectionViewCell.swift
 //  iOS9-Astronomy
 //
 //  Created by Jake Connerly on 11/12/19.
 //  Copyright © 2019 jake connerly. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+import UIKit
 
-@interface ViewController : UIViewController
-
-
-@end
-
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+}
