375030306107312e83a3141fd7948ec95757509a
Isvvc <lyons@tuta.io>
2019-12-16 13:51:36 -0700

    Create blank Xcode project

---
+++ Astronomy-Hybrid/Astronomy-Hybrid/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Astronomy-Hybrid
+//
+//  Created by Isaac Lyons on 12/16/19.
+//  Copyright © 2019 Isaac Lyons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
