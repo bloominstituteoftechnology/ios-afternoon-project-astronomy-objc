415175b45283c7f20ab7c63b4662cd7b8f248c01
Isvvc <lyons@tuta.io>
2019-12-16 14:36:17 -0700

    Create MarsPhotoReference Swift class

---
+++ Astronomy-Hybrid/Astronomy-Hybrid/Model/MarsPhotoReference.swift
@@ -0,0 +1,35 @@
+//
+//  MarsPhotoReference.swift
+//  Astronomy-Hybrid
+//
+//  Created by Isaac Lyons on 12/16/19.
+//  Copyright © 2019 Isaac Lyons. All rights reserved.
+//
+
+import Foundation
+
+@objc class MarsPhotoReference: NSObject {
+    @objc let id: Int
+    @objc let sol: Int
+    @objc let imageURL: URL
+    
+    @objc init(id: Int, sol: Int, imageURL: URL) {
+        self.id = id
+        self.sol = sol
+        self.imageURL = imageURL
+    }
+    
+    @objc init?(with dictionary: [String: Any]) {
+        guard let id = dictionary["id"] as? Int,
+            let sol = dictionary["sol"] as? Int,
+            let imageString = dictionary["img_src"] as? String,
+            let imageURL = URL(string: imageString) else {
+            return nil
+        }
+        
+        self.id = id
+        self.sol = sol
+        self.imageURL = imageURL
+        super.init()
+    }
+}
