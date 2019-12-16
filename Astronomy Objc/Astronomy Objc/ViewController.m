472ec1e19303aafaa2628cea41e8e205eabf70e6
Gi Pyo Kim <gipyok@gmail.com>
2019-12-16 13:00:40 -0800

    initial commit

---
+++ Astronomy Objc/Astronomy Objc/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Astronomy Objc
+//
+//  Created by Gi Pyo Kim on 12/16/19.
+//  Copyright © 2019 GIPGIP Studio. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
