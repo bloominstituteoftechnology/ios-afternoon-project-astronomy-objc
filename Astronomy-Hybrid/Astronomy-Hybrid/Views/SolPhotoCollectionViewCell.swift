897883f4e729e3a86b69a160496ece8b21435563
Skyler Suzuki <skylersuzuk1@gmail.com>
2020-01-27 13:21:57 -0800

    Added collectionview and viewcontroller to storyboard. Hooked up to base classes in swift.

---
+++ Astronomy-Hybrid/Astronomy-Hybrid/Views/SolPhotoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  SolPhotoCollectionViewCell.swift
+//  Astronomy-Hybrid
+//
+//  Created by Lambda_School_Loaner_204 on 1/27/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class SolPhotoCollectionViewCell: UICollectionViewCell {
+    
+    
+}
