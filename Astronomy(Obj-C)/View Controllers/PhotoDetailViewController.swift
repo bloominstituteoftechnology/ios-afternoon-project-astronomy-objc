c474e8890ec1c543df675187624b7e97256b436d
Jon Bash <jonbash@protonmail.com>
2020-01-27 08:38:05 -0800

    add VC files, bridging header

---
+++ Astronomy(Obj-C)/View Controllers/PhotoDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoDetailViewController.swift
+//  Astronomy(Obj-C)
+//
+//  Created by Jon Bash on 2020-01-27.
+//  Copyright © 2020 Jon Bash. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
