008d69c6a015a6314b517e0939afaee7afb74388
Jon Bash <jonbash@protonmail.com>
2020-01-27 07:07:44 -0800

    init xcode project

---
+++ Astronomy(Obj-C)/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Astronomy(Obj-C)
+//
+//  Created by Jon Bash on 2020-01-27.
+//  Copyright © 2020 Jon Bash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
