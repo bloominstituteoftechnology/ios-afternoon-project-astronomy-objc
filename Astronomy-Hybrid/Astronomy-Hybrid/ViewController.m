0113c16a88e40f71e3b2f830667646db0a5d1412
Jerry Haaser <jerry.haaser@hotmail.com>
2020-01-27 12:41:34 -0600

    added starter project

---
+++ Astronomy-Hybrid/Astronomy-Hybrid/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Astronomy-Hybrid
+//
+//  Created by Jerry haaser on 1/27/20.
+//  Copyright © 2020 Jerry haaser. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
