ebb55d80c67d33f838010281ef15330e0701d439
Jon Bash <jonbash@protonmail.com>
2020-01-27 14:34:12 -0800

    add collectionViewCell

---
+++ Astronomy(Obj-C)/Views/JBPhotoCollectionViewCell.h
@@ -0,0 +1,20 @@
+//
+//  JBPhotoCollectionViewCell.h
+//  Astronomy(Obj-C)
+//
+//  Created by Jon Bash on 2020-01-27.
+//  Copyright © 2020 Jon Bash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+NS_SWIFT_NAME(PhotoCollectionViewCell)
+@interface JBPhotoCollectionViewCell : UICollectionViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Astronomy(Obj-C)/Views/JBPhotoCollectionViewCell.m
@@ -0,0 +1,14 @@
+//
+//  JBPhotoCollectionViewCell.m
+//  Astronomy(Obj-C)
+//
+//  Created by Jon Bash on 2020-01-27.
+//  Copyright © 2020 Jon Bash. All rights reserved.
+//
+
+#import "JBPhotoCollectionViewCell.h"
+
+@implementation JBPhotoCollectionViewCell
+
+
+@end
