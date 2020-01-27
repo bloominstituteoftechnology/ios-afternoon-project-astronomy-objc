dba7da2173a56bb7f0fdde76ef24cb3565253cdf
Jon Bash <jonbash@protonmail.com>
2020-01-27 08:54:26 -0800

    set up mainVC titleView

--- Astronomy(Obj-C)/View Controllers/MainCollectionViewController.swift
+++ Astronomy(Obj-C)/View Controllers/MainCollectionViewController.swift
@@ -12,6 +12,10 @@ private let reuseIdentifier = "Cell"
 
 class MainCollectionViewController: UICollectionViewController {
 
+    private let solLabel = UILabel()
+
+    // MARK: - View Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -24,7 +28,31 @@ class MainCollectionViewController: UICollectionViewController {
         // Do any additional setup after loading the view.
     }
 
-    /*
+    private func configureTitleView() {
+        let font = UIFont.systemFont(ofSize: 30)
+        let attributes = [NSAttributedString.Key.font: font]
+
+        let prevTitle = NSAttributedString(string: "<", attributes: attributes)
+        let prevButton = UIButton(type: .system)
+        prevButton.accessibilityIdentifier = "MainCollectionViewController.PreviousSolButton"
+        prevButton.setAttributedTitle(prevTitle, for: .normal)
+        prevButton.addTarget(self, action: #selector(goToPreviousSol(_:)), for: .touchUpInside)
+
+        let nextTitle = NSAttributedString(string: ">", attributes: attributes)
+        let nextButton = UIButton(type: .system)
+        nextButton.setAttributedTitle(nextTitle, for: .normal)
+        nextButton.addTarget(self, action: #selector(goToNextSol(_:)), for: .touchUpInside)
+        nextButton.accessibilityIdentifier = "MainCollectionViewController.NextSolButton"
+
+        let stackView = UIStackView(arrangedSubviews: [prevButton, solLabel, nextButton])
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.spacing = UIStackView.spacingUseSystem
+
+        navigationItem.titleView = stackView
+    }
+
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -32,7 +60,6 @@ class MainCollectionViewController: UICollectionViewController {
         // Get the new view controller using [segue destinationViewController].
         // Pass the selected object to the new view controller.
     }
-    */
 
     // MARK: UICollectionViewDataSource
 
@@ -85,5 +112,17 @@ class MainCollectionViewController: UICollectionViewController {
     
     }
     */
+
+    // MARK: - Private Helpers
+
+    @objc
+    private func goToPreviousSol(_ sender: Any) {
+
+    }
+
+    @objc
+    private func goToNextSol(_ sender: Any) {
+
+    }
 
 }
