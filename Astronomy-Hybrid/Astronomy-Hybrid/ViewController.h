c79d08d0a007c1328a651170d877c4b1564ab2c1
Skyler Suzuki <skylersuzuk1@gmail.com>
2020-01-27 10:41:37 -0800

    Added starter project.

---
+++ Astronomy-Hybrid/Astronomy-Hybrid/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Astronomy-Hybrid
+//
+//  Created by Lambda_School_Loaner_204 on 1/27/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
