a79c860c65902e5254e7deb291c6133c9a277274
Dennis Rudolph <Dennis@Denniss-MacBook-Pro.local>
2020-01-27 19:08:59 -0500

    Added Swift Files

---
+++ Astronomy/Astronomy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Astronomy
+//
+//  Created by Dennis Rudolph on 1/27/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Astronomy/Astronomy/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Astronomy
+//
+//  Created by Dennis Rudolph on 1/27/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
