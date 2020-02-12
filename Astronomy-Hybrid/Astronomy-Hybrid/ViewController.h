d2266822df6fdaf93140859b5eff5459afd8c936
Vici Shaweddy <vshaweddy@gmail.com>
2020-02-11 21:15:55 -0500

    added starter project

---
+++ Astronomy-Hybrid/Astronomy-Hybrid/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Astronomy-Hybrid
+//
+//  Created by Vici Shaweddy on 2/11/20.
+//  Copyright © 2020 Vici Shaweddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Astronomy-Hybrid/Astronomy-Hybrid/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Astronomy-Hybrid
+//
+//  Created by Vici Shaweddy on 2/11/20.
+//  Copyright © 2020 Vici Shaweddy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
