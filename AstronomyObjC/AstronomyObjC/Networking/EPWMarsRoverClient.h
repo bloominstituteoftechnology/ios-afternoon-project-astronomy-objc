f65f64dc49c2fb2fed452c2a01f0e8095a065e5f
elizabeth paige <wingateep@Icloud.com>
2020-04-20 18:48:45 -0400

    MarsRoverClient

---
+++ AstronomyObjC/AstronomyObjC/Networking/EPWMarsRoverClient.h
@@ -0,0 +1,17 @@
+//
+//  EPWMarsRoverClient.h
+//  AstronomyObjC
+//
+//  Created by Elizabeth Wingate on 4/20/20.
+//  Copyright © 2020 Lambda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface EPWMarsRoverClient : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AstronomyObjC/AstronomyObjC/Networking/EPWMarsRoverClient.m
@@ -0,0 +1,16 @@
+//
+//  EPWMarsRoverClient.m
+//  AstronomyObjC
+//
+//  Created by Elizabeth Wingate on 4/20/20.
+//  Copyright © 2020 Lambda. All rights reserved.
+//
+
+#import "EPWMarsRoverClient.h"
+
+@implementation EPWMarsRoverClient
+
+static NSString *const baseURLString = @"https://api.nasa.gov/mars-photos/api/v1";
+static NSString *const apiKey = @"LetSPBmsYEe3N7AwFzpnusJkAYzHYYersNAv16It";
+
+@end
