e45af285b79d6ab63ef7186d7dc32004a9221410
elizabeth paige <wingateep@Icloud.com>
2020-04-20 16:30:47 -0400

    Made Project

---
+++ AstronomyObjC/AstronomyObjC/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  AstronomyObjC
+//
+//  Created by Elizabeth Wingate on 4/20/20.
+//  Copyright © 2020 Lambda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
