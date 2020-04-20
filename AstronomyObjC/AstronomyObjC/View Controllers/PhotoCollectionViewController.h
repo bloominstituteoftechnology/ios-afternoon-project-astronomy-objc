75b781544ca1c3869c4a3bef494c8322a0233f28
elizabeth paige <wingateep@Icloud.com>
2020-04-20 18:06:35 -0400

    PhotoDetailViewController + PhotoCollectionViewController

---
+++ AstronomyObjC/AstronomyObjC/View Controllers/PhotoCollectionViewController.h
@@ -0,0 +1,17 @@
+//
+//  PhotoCollectionViewController.h
+//  AstronomyObjC
+//
+//  Created by Elizabeth Wingate on 4/20/20.
+//  Copyright © 2020 Lambda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PhotoCollectionViewController : UICollectionViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
