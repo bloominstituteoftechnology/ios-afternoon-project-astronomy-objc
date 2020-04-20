751a9a0f9c7e5c6a2ade50bacdf5da4ec9f10a9b
Enrique Gongora <EGTech24@gmail.com>
2020-04-20 15:03:34 -0700

    Created folders. Created bridging header. Created Rover file.

---
+++ AstronomyObjectiveC/AstronomyObjectiveC/Model Controller/APIController.h
@@ -0,0 +1,17 @@
+//
+//  APIController.h
+//  AstronomyObjectiveC
+//
+//  Created by Enrique Gongora on 4/20/20.
+//  Copyright © 2020 Enrique Gongora. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface APIController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AstronomyObjectiveC/AstronomyObjectiveC/Model Controller/APIController.m
@@ -0,0 +1,13 @@
+//
+//  APIController.m
+//  AstronomyObjectiveC
+//
+//  Created by Enrique Gongora on 4/20/20.
+//  Copyright © 2020 Enrique Gongora. All rights reserved.
+//
+
+#import "APIController.h"
+
+@implementation APIController
+
+@end
