aba610f040d61cc09b60143956d15d8942d64896
Nick <ptnguyen1901@gmail.com>
2020-04-21 07:11:15 -0400

    Refractored and organized files.

--- AstronomyObjC/AstronomyObjC/DetailViewController.swift
+++ AstronomyObjC/AstronomyObjC/DetailViewController.swift
@@ -10,23 +10,26 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    //MARK:- Outlets
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var dateTakenLabel: UILabel!
     @IBOutlet weak var cameraLabel: UILabel!
     @IBOutlet weak var saveButton: UIButton!
-    @IBAction func savePressed(_ sender: UIButton) {
-        
-    }
     
-    var fetcher = SolFetcher()
     
-    lazy var dateFormatter: DateFormatter = {
+    @IBAction func savePressed(_ sender: UIButton) {  }
+    //MARK:- Properties
+    
+    private var fetcher = SolFetcher()
+    
+    private lazy var dateFormatter: DateFormatter = {
        let dm = DateFormatter()
         dm.calendar = .current
         dm.dateStyle = .short
         return dm
     }()
+    //MARK:- Life cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,6 +37,7 @@ class DetailViewController: UIViewController {
         fetcher.fetchOneSingleSol { (sols, _) in
             sols?.forEach({ (sol) in
                 DispatchQueue.main.async {
+                    
                     self.cameraLabel.text = "Camera: \(sol.camera.name)"
                     self.dateTakenLabel.text = "Taken by \(sol.idNumber) on  \(self.dateFormatter.string(from: sol.earthDate)) Sol \(sol.sol)"
                     self.imageView.load(url: (URL(string: sol.imageURL)?.usingHTTPS!)!)
@@ -45,25 +49,4 @@ class DetailViewController: UIViewController {
     }
 
 
-}
-
-extension UIImageView {
-    func load(url: URL) {
-        DispatchQueue.global().async { [weak self] in
-            if let data = try? Data(contentsOf: url) {
-                if let image = UIImage(data: data) {
-                    DispatchQueue.main.async {
-                        self?.image = image
-                    }
-                }
-            }
-        }
-    }
-}
-extension URL {
-    var usingHTTPS: URL? {
-        guard var components = URLComponents(url: self, resolvingAgainstBaseURL: true) else { return nil }
-        components.scheme = "https"
-        return components.url
-    }
 }
