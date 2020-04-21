97b2ff3478f265182bfbb94cde1ca3435a014fff
Chris <chris.gonzales.82@gmail.com>
2020-04-21 12:44:47 -0700

    Sol Swift file implemented

--- AstronomyObjC/AstronomyObjC/Models/Sol.swift
+++ AstronomyObjC/AstronomyObjC/Models/Sol.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+
+class Sol: NSObject {
+    
+    // MARK: - Properties
+    @objc let sol: Int
+    @objc let totalPhotos: Int
+    @objc let cameras: [String]
+    
+    // MARK: - Initializers
+    
+    init(sol: Int,
+         totalPhotos: Int,
+         cameras: [String]) {
+        self.sol = sol
+        self.totalPhotos = totalPhotos
+        self.cameras = cameras
+    }
+    
+    @objc convenience init?(dictionary: [String: Any]) {
+        guard let sol = dictionary["sol"] as? Int,
+            let totalPhotos = dictionary["total_photos"] as? Int,
+            let cameras = dictionary["cameras"] as? String .ExtendedGraphemeClusterLiteralType else {
+                return nil
+        }
+        self.init(sol: sol,
+                  totalPhotos: totalPhotos,
+                  cameras: cameras)
+    }
+}
