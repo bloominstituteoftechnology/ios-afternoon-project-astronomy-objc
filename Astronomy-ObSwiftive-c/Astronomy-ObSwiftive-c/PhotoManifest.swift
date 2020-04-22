9a3b0b57b79f15333d1467241002d4582b480db4
Craig Swanson <cswanson02@me.com>
2020-04-21 23:43:50 -0500

    Update PhotoManifest.swift
    Updated PhotoManifest model to provide an array of valid sol dates.

--- Astronomy-ObSwiftive-c/Astronomy-ObSwiftive-c/PhotoManifest.swift
+++ Astronomy-ObSwiftive-c/Astronomy-ObSwiftive-c/PhotoManifest.swift
@@ -15,13 +15,22 @@ import Foundation
     @objc let max_date: Date
     @objc let total_photos: Int
     @objc var photos: [[String : Any]]
+    @objc var sols: [Int]?
     
-    @objc init(name: String, max_sol: Int, max_date: Date, total_photos: Int, photos: [[String : Any]]) {
+    @objc init(name: String, max_sol: Int, max_date: Date, total_photos: Int, photos: [[String : Any]], sols: [Int]?) {
         self.name = name
         self.max_sol = max_sol
         self.max_date = max_date
         self.total_photos = total_photos
         self.photos = photos
+        self.sols = {
+            var solArray: [Int] = []
+            for sol in photos {
+                let solDate = sol["sol"] as! Int
+                solArray.append(solDate)
+            }
+            return solArray
+        }()
     }
     
     @objc convenience init?(dictionary: [String: [String: Any]]) {
@@ -38,8 +47,16 @@ import Foundation
         let maxDate = dateFormatter.date(from: jsonDate),
             let solPhotos = dictionaryValues!["photos"],
         let totalPhotos = dictionaryValues!["total_photos"] as? Int else { return nil }
+        let solData: [Int] = {
+            var solArray: [Int] = []
+            for sol in solPhotos as! [[String : Any]] {
+                let solDate = sol["sol"] as! Int
+                solArray.append(solDate)
+            }
+            return solArray
+        }()
         
-        self.init(name: roverName, max_sol: maxSol, max_date: maxDate, total_photos: totalPhotos, photos: solPhotos as? [[String : Any]] ?? [])
+        self.init(name: roverName, max_sol: maxSol, max_date: maxDate, total_photos: totalPhotos, photos: solPhotos as? [[String : Any]] ?? [], sols: solData)
     }
     
     @objc static let dateFormatter: DateFormatter = {
@@ -49,10 +66,4 @@ import Foundation
         formatter.dateFormat = "yyyy-MM-dd"
         return formatter
     }()
-    
-    static var jsonDecoderDateFormatter: JSONDecoder {
-        let result = JSONDecoder()
-        result.dateDecodingStrategy = .formatted(dateFormatter)
-        return result
-    }
 }
