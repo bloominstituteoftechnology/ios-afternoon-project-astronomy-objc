340bfaf038565159b661ec4534ba0139ea14ee6e
Ufuk Türközü <ufuk_trkz@hotmail.de>
2020-04-22 17:03:33 +0200

    Added UFOPhotoCollectionViewCell

---
+++ Astronomy - ObjC/Astronomy - ObjC/ViewController/UFOPhotoCollectionViewCell.h
@@ -0,0 +1,19 @@
+//
+//  UFOPhotoCollectionViewCell.h
+//  Astronomy - ObjC
+//
+//  Created by Ufuk Türközü on 21.04.20.
+//  Copyright © 2020 Ufuk Türközü. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UFOPhotoCollectionViewCell : UICollectionViewCell
+
+@property (nonatomic, strong) IBOutlet UIImageView *imageView;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Astronomy - ObjC/Astronomy - ObjC/ViewController/UFOPhotoCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  UFOPhotoCollectionViewCell.m
+//  Astronomy - ObjC
+//
+//  Created by Ufuk Türközü on 21.04.20.
+//  Copyright © 2020 Ufuk Türközü. All rights reserved.
+//
+
+#import "UFOPhotoCollectionViewCell.h"
+
+@implementation UFOPhotoCollectionViewCell
+
+@end
