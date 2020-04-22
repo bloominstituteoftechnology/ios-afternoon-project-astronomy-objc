b292bfda219b30503e3e52c7654710d4b4332170
Craig Swanson <cswanson02@me.com>
2020-04-22 12:20:32 -0500

    PhotoDetailViewController
    Successfully implemented code to show rover image in detail view controller.

--- Astronomy-ObSwiftive-c/Astronomy-ObSwiftive-c/RoverPhotoDetailViewController.swift
+++ Astronomy-ObSwiftive-c/Astronomy-ObSwiftive-c/RoverPhotoDetailViewController.swift
@@ -10,10 +10,36 @@ import UIKit
 
 class RoverPhotoDetailViewController: UIViewController {
 
+    @IBOutlet var roverPhotoImage: UIImageView!
+    
+    var photoController = PhotoController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        photoController.fetchRoverPhotos("curiosity", 12) { (data, error) in
+            guard let data = data else { return }
+            print("Data from sol 12, first Image: \(data.roverPhotos[0].photoURL)")
+            print("Total photos: \(data.roverPhotos.count)")
+            print(data.roverPhotos[0].photoID)
+            print(data.roverPhotos[0].sol)
+            print(data.roverPhotos[0].cameraName)
+            print("Data from sol 12, 32nd Image: \(data.roverPhotos[31].photoURL)")
+            print("Total photos: \(data.roverPhotos.count)")
+            print(data.roverPhotos[31].photoID)
+            print(data.roverPhotos[31].sol)
+            print(data.roverPhotos[31].cameraName)
+            
+            DispatchQueue.main.async {
+                do {
+                    let photoData = try Data(contentsOf: data.roverPhotos[31].photoURL)
+                    self.roverPhotoImage.image = UIImage(data: photoData)
+                } catch {
+                    NSLog("Error setting up views on detail view controller: \(error)")
+                }
+            }
+            return
+        }
     }
     
 
