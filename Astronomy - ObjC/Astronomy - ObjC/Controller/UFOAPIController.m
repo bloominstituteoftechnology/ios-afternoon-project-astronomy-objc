799bd1c5b59303308457dd37ba29bd8dc5dc4560
Ufuk Türközü <ufuk_trkz@hotmail.de>
2020-04-22 17:25:46 +0200

    fetchPhotosWithRoverName works!

--- Astronomy - ObjC/Astronomy - ObjC/Controller/UFOAPIController.m
+++ Astronomy - ObjC/Astronomy - ObjC/Controller/UFOAPIController.m
@@ -14,7 +14,6 @@
 - (void)fetchPhotoWithRoverName:(NSString *)roverName
                             sol:(int)sol
                      completion: (void(^)(NSArray<UFOMarsPhoto *> *photos))completion {
-    
     static NSString *baseURLString = @"https://api.nasa.gov/mars-photos/api/v1";
     static NSString *apiKey = @"hjWKVcrc8T02tVqAgdqg57xCpNKEitiQlWDEbtXb";
     NSURL *url = [NSURL URLWithString:baseURLString];
@@ -60,10 +59,6 @@
             return;
         }
         
-//        UFOMarsPhoto *photos = [[UFOMarsPhoto alloc ] initWithDictionary:dictionary];
-//        completion(photos);
-//        NSLog(@"PHOTOS: %@", photos);
-        
         NSDictionary *jsonDict = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
         if (!jsonDict || ![jsonDict isKindOfClass:[NSDictionary class]]) {
             if (error) {
@@ -78,52 +73,51 @@
             UFOMarsPhoto *photo = [[UFOMarsPhoto alloc] initWithDictionary:dict];
             if (photo) {
                 [photos addObject:photo];
+                NSLog(@"PHOTOS: %@", photos);
             }
         }
         completion(photos);
-        
     }] resume];
-    
 }
 
-- (void)fetchSinglePhoto:(void(^)(UFOMarsPhoto *, NSError *error))completion {
-    
-    NSURL *requestUrl = @"http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/00012/opgs/edr/ccam/CR0_398560983EDR_F0030004CCAM03012M_.JPG";
-    
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:requestUrl];
-    
-    [[[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-        
-        if (error) {
-            NSLog(@"Error fetching photo: %@", error);
-            completion(nil, error);
-            return;
-        }
-        
-        if (!data) {
-            NSLog(@"No data: %@", data);
-            completion(nil, error);
-            return;
-        }
-        
-        error = nil;
-        NSDictionary *dictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
-        if (error) {
-            NSLog(@"Error decoding JSON from data: %@", error);
-            completion(nil, error);
-            return;
-        }
-        
-        if (!dictionary || ![dictionary isKindOfClass:[NSDictionary class]]) {
-            NSLog(@"expected dictionary: %@", error);
-            completion(nil, error);
-            return;
-        }
-        
-        UFOMarsPhoto *photo = [[UFOMarsPhoto alloc] initWithDictionary:dictionary];
-        completion(photo, nil);
-        
-    }] resume];
-}
+//- (void)fetchSinglePhoto:(void(^)(UFOMarsPhoto *, NSError *error))completion {
+//    
+//    NSURL *requestUrl = @"http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/00012/opgs/edr/ccam/CR0_398560983EDR_F0030004CCAM03012M_.JPG";
+//    
+//    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:requestUrl];
+//    
+//    [[[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+//        
+//        if (error) {
+//            NSLog(@"Error fetching photo: %@", error);
+//            completion(nil, error);
+//            return;
+//        }
+//        
+//        if (!data) {
+//            NSLog(@"No data: %@", data);
+//            completion(nil, error);
+//            return;
+//        }
+//        
+//        error = nil;
+//        NSDictionary *dictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
+//        if (error) {
+//            NSLog(@"Error decoding JSON from data: %@", error);
+//            completion(nil, error);
+//            return;
+//        }
+//        
+//        if (!dictionary || ![dictionary isKindOfClass:[NSDictionary class]]) {
+//            NSLog(@"expected dictionary: %@", error);
+//            completion(nil, error);
+//            return;
+//        }
+//        
+//        UFOMarsPhoto *photo = [[UFOMarsPhoto alloc] initWithDictionary:dictionary];
+//        completion(photo, nil);
+//        
+//    }] resume];
+//}
 
 @end
--- Astronomy - ObjC/Astronomy - ObjC/ViewController/DetailViewController.swift
+++ Astronomy - ObjC/Astronomy - ObjC/ViewController/DetailViewController.swift
@@ -65,15 +65,17 @@ class DetailViewController: UIViewController {
     }
     
     private func updateViews() {
-        guard let photo = photo, isViewLoaded else { return }
-        do {
-            let data = try Data(contentsOf: photo.imgSrc.usingHTTPS!)
-            solPhotoIV.image = UIImage(data: data)
-            let dateString = dateFormatter.string(from: photo.earthDate)
-            detailLabel.text = "Taken by \(photo.camera.roverId) on \(dateString) (Sol \(photo.sol))"
-            cameraLabel.text = photo.camera.fullName
-        } catch {
-            NSLog("Error setting up views on detail view controller: \(error)")
+        DispatchQueue.main.async {
+            guard let photo = self.photo, self.isViewLoaded else { return }
+            do {
+                let data = try Data(contentsOf: photo.imgSrc.usingHTTPS!)
+                self.solPhotoIV.image = UIImage(data: data)
+                let dateString = self.dateFormatter.string(from: photo.earthDate)
+                self.detailLabel.text = "Taken by \(photo.camera.roverId) on \(dateString) (Sol \(photo.sol))"
+                self.cameraLabel.text = photo.camera.fullName
+            } catch {
+                NSLog("Error setting up views on detail view controller: \(error)")
+            }
         }
     }
 
