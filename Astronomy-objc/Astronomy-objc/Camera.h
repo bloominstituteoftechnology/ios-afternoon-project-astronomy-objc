d738f8c486f534da2d3c80bfc8cfee16561db878
dillonp23 <dillonp.lambda@gmail.com>
2020-04-26 19:56:39 -0700

    Added Objective-C data models for Camera and Rover

--- Astronomy-objc/Astronomy-objc/Camera.h
+++ Astronomy-objc/Astronomy-objc/Camera.h
@@ -12,6 +12,9 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface Camera : NSObject
 
+@property (nonatomic, readonly, copy) NSString *name;
+@property (nonatomic, readonly, copy) NSString *fullName;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- Astronomy-objc/Astronomy-objc/Rover.h
+++ Astronomy-objc/Astronomy-objc/Rover.h
@@ -7,10 +7,21 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Camera.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface Rover : NSObject
+
+@property (nonatomic, readonly) int identifier; // just "id" from the api
+@property (nonatomic, readonly, copy) NSString *name;
+@property (nonatomic, readonly) NSDate *landingDate; // NSDate from NSString
+@property (nonatomic, readonly) NSDate *launchDate; // NSDate from NSString
+@property (nonatomic, readonly, copy) NSString *status;
+@property (nonatomic, readonly) int maxSol;
+@property (nonatomic, readonly) NSDate *maxDate; // NSDate from NSString
+@property (nonatomic, readonly) int totalPhotos;
+@property (nonatomic, readonly, copy) NSArray<Camera *> *cameras;
 
 @end
 
