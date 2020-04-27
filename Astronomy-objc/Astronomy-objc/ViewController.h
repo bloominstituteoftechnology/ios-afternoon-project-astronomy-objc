f017f18e1d24f3fdf790e98f171c2f55a36a500f
dillonp23 <dillonp.lambda@gmail.com>
2020-04-26 18:25:46 -0700

    Added Xcode starter project

---
+++ Astronomy-objc/Astronomy-objc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Astronomy-objc
+//
+//  Created by Dillon P on 4/26/20.
+//  Copyright © 2020 Dillon's Lambda Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Astronomy-objc/Astronomy-objc/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Astronomy-objc
+//
+//  Created by Dillon P on 4/26/20.
+//  Copyright © 2020 Dillon's Lambda Team. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
