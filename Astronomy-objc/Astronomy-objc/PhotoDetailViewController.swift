3ba3df2990f8ec84f11f2e4b74be7afe90d73bad
dillonp23 <dillonp.lambda@gmail.com>
2020-04-28 10:07:22 -0700

    Renamed MarsRovePhoto imageView to MarsRoverImage. Added save photo action blueprint. Set up UI based on a selected MarsRoverPhoto passed from the collection view.

--- Astronomy-objc/Astronomy-objc/PhotoDetailViewController.swift
+++ Astronomy-objc/Astronomy-objc/PhotoDetailViewController.swift
@@ -10,44 +10,46 @@ import UIKit
 
 class PhotoDetailViewController: UIViewController {
     
-    @IBOutlet weak var marsRoverPhoto: UIImageView!
+    @IBOutlet weak var marsRoverImage: UIImageView!
     @IBOutlet weak var photoDetailsLabel: UILabel!
     @IBOutlet weak var cameraDetailsLabel: UILabel!
     @IBOutlet weak var savePhotoButton: UIButton!
     
-    
+    var marsRoverPhoto: MarsRoverPhoto?
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let marsRoverManifestController = MarsRoverController()
         
-        marsRoverManifestController.fetchSingleImage("https://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/00030/opgs/edr/ncam/NRA_400165599EDR_F0040000NCAM00106M_.JPG") { (image, error) in
-            if let error = error {
-                print(error)
-            }
-            
-            if let image = image {
-                self.marsRoverPhoto.image = image
-            }
-        }
-        // Do any additional setup after loading the view.
+        guard let marsRoverPhoto = marsRoverPhoto else { return }
+        
+        let roverId = marsRoverPhoto.rover.identifier
+        let roverName = marsRoverPhoto.rover.name
+        let earthDate = marsRoverPhoto.earthDate
+        let sol = marsRoverPhoto.sol
+        
+        let cameraName = marsRoverPhoto.camera.fullName
+        
+        photoDetailsLabel.text = "Taken by Rover #\(roverId) (\(roverName)) on \(earthDate) (\(sol))"
+        cameraDetailsLabel.text = "Camera: \(cameraName)"
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func savePhotoToLibrary(UIImage: UIImage) {
+        // TODO: Request permission to users Photos library
+        
+        // TODO: Save photo to library
+        
+        // TODO: Present alert for confirmation
     }
-    */
     
     // MARK: - Actions
     
     @IBAction func savePhotoButtonTapped(_ sender: Any) {
+        if let image = marsRoverImage.image {
+            savePhotoToLibrary(UIImage: image)
+        }
     }
     
 }
