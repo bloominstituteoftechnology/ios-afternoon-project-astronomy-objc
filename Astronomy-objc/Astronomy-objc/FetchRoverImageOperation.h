6f49a3b3b5fce4e9689ff67bea4b9abbbb53ec66
dillonp23 <dillonp.lambda@gmail.com>
2020-04-29 16:39:31 -0700

    Added FetchRoverImageOperation class

---
+++ Astronomy-objc/Astronomy-objc/FetchRoverImageOperation.h
@@ -0,0 +1,21 @@
+//
+//  FetchRoverImageOperation.h
+//  Astronomy-objc
+//
+//  Created by Dillon P on 4/29/20.
+//  Copyright © 2020 Dillon's Lambda Team. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FetchRoverImageOperation : NSOperation
+
+@property (nonatomic, copy) NSString *imgSrcString;
+
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Astronomy-objc/Astronomy-objc/FetchRoverImageOperation.m
@@ -0,0 +1,31 @@
+//
+//  FetchRoverImageOperation.m
+//  Astronomy-objc
+//
+//  Created by Dillon P on 4/29/20.
+//  Copyright © 2020 Dillon's Lambda Team. All rights reserved.
+//
+
+#import "FetchRoverImageOperation.h"
+
+@implementation FetchRoverImageOperation
+
+- (BOOL)isAsynchronous
+{
+    return YES;
+}
+
+- (void)start
+{
+    
+}
+
+- (void)cancel
+{
+    
+}
+
+
+
+
+@end
