7cbc64e289a75d54e2f5f7a2173658886921b3b3
dillonp23 <dillonp.lambda@gmail.com>
2020-05-01 14:45:19 -0700

    Added a generic cache model. Updated values in main.storyboard

---
+++ Astronomy-objc/Astronomy-objc/Cache.m
@@ -0,0 +1,51 @@
+//
+//  Cache.m
+//  Astronomy-objc
+//
+//  Created by Dillon P on 4/30/20.
+//  Copyright © 2020 Dillon's Lambda Team. All rights reserved.
+//
+
+#import "Cache.h"
+
+@interface Cache()
+
+@property NSMutableDictionary *cache;
+@property dispatch_queue_t cacheQueue;
+
+@end
+
+@implementation Cache
+
+- (instancetype)initWithKey:(NSString *)key value:(id)value
+{
+    if (self = [super init]) {
+        self.cache = [[NSMutableDictionary alloc] initWithObjectsAndKeys:value, key, nil];
+        self.cacheQueue = dispatch_queue_create("com.astronomy.cacheQueue", NULL);
+    }
+    return self;
+}
+
+- (void)cacheValue:(id)value forKey:(NSString *)key
+{
+    dispatch_async(self.cacheQueue, ^{
+        [self.cache setValue:value forKey:key];
+    });
+}
+
+- (_Nullable id)valueForKey:(NSString *)key
+{
+    dispatch_sync(self.cacheQueue, ^{
+        _value = [self.cache valueForKey:key];
+    });
+    return _value;
+}
+
+-(void)clear
+{
+    dispatch_async(self.cacheQueue, ^{
+        [self.cache removeAllObjects];
+    });
+}
+
+@end
