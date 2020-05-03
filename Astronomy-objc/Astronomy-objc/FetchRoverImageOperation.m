bb991b9f57d67644357edbe0341cfb60de8b0a77
dillonp23 <dillonp.lambda@gmail.com>
2020-05-02 20:10:07 -0700

    Fixed bug where app may crash when quickly switching between multiple sols.

--- Astronomy-objc/Astronomy-objc/FetchRoverImageOperation.m
+++ Astronomy-objc/Astronomy-objc/FetchRoverImageOperation.m
@@ -14,6 +14,7 @@
 // isExecuting and isFinshed properties - these ARE atomic BOOL's as they will be accessed on different threads
 @property BOOL internalIsExecuting;
 @property BOOL internalIsFinished;
+@property BOOL internalIsCancelled;
 
 @property NSURLSessionDataTask *dataTask;
 @property NSURLSession *urlSession;
@@ -39,6 +40,11 @@
 
 - (void)start
 {
+    if (self.internalIsCancelled) {
+        self.internalIsFinished = YES;
+        return;
+    }
+    
     self.internalIsExecuting = YES;
     
     NSURL *url = [[NSURL alloc] initWithString:self.imgURLString];
@@ -46,16 +52,19 @@
     NSURLSessionDataTask *task = [self.urlSession dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
         if (error) {
             NSLog(@"Error fetching image: %@", error);
+            self.internalIsExecuting = NO;
             self.internalIsFinished = YES;
             return;
         }
         
         if (!data) {
             NSLog(@"No data returned from data task");
+            self.internalIsExecuting = NO;
             self.internalIsFinished = YES;
             return;
         } else {
             self.imgData = data;
+            self.internalIsExecuting = NO;
             self.internalIsFinished = YES;
             return;
         }
@@ -68,20 +77,28 @@
 
 - (void)cancel
 {
+    if (self.internalIsExecuting) {
+        self.internalIsExecuting = NO;
+        self.internalIsFinished = YES;
+    }
     [self.dataTask cancel];
-    [super cancel];
+    self.internalIsCancelled = YES;
 }
 
+- (BOOL)isCancelled
+{
+    return self.internalIsCancelled;
+}
 
 - (BOOL)isExecuting
 {
-    return _internalIsExecuting;
+    return self.internalIsExecuting;
 }
 
 
 - (BOOL)isFinished
 {
-    return _internalIsFinished;
+    return self.internalIsFinished;
 }
 
 
@@ -95,6 +112,11 @@
 + (NSSet<NSString *> *)keyPathsForValuesAffectingIsFinished
 {
     return [NSSet setWithObject:@"internalIsFinished"];
+}
+
++ (NSSet<NSString *> *)keyPathsForValuesAffectingInternalIsCancelled
+{
+    return [NSSet setWithObject:@"internalIsCancelled"];
 }
 
 
