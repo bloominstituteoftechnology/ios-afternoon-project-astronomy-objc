456e51f4395dfc6ba0e609c6836dc45a018c2d93
David Wright <davidtwright89@gmail.com>
2020-06-03 01:17:09 -0400

    Added an LSIMarsRoverClient class

---
+++ Astronomy/Astronomy/Networking/LSIMarsRoverClient.h
@@ -0,0 +1,17 @@
+//
+//  LSIMarsRoverClient.h
+//  Astronomy
+//
+//  Created by David Wright on 6/3/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIMarsRoverClient : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Astronomy/Astronomy/Networking/LSIMarsRoverClient.m
@@ -0,0 +1,13 @@
+//
+//  LSIMarsRoverClient.m
+//  Astronomy
+//
+//  Created by David Wright on 6/3/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import "LSIMarsRoverClient.h"
+
+@implementation LSIMarsRoverClient
+
+@end
