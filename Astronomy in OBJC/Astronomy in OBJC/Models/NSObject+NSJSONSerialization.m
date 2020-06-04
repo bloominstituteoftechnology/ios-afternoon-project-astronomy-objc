bac5251b2a1aaed7334709aa2c2fcd331d4747f4
Sal <salmanamer@gmail.com>
2020-06-04 04:21:05 -0700

    created and MarsPhotos+NSJSONSerialization.h

---
+++ Astronomy in OBJC/Astronomy in OBJC/Models/NSObject+NSJSONSerialization.m
@@ -0,0 +1,14 @@
+//
+//  NSObject+NSJSONSerialization.m
+//  Astronomy-in-OBJC
+//
+//  Created by Sal B Amer on 6/4/20.
+//  Copyright © 2020 Sal B Amer. All rights reserved.
+//
+
+#import "NSObject+NSJSONSerialization.h"
+#import "MarsPhotos.h"
+
+@implementation NSObject (NSJSONSerialization)
+
+@end
