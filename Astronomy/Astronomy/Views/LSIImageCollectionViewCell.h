6cd7aacc05768512f542e5b0260c6187e9b33468
David Wright <davidtwright89@gmail.com>
2020-06-03 23:25:28 -0400

    Added an Objective-C collectionView cell class: LSIImageCollectionViewCell

---
+++ Astronomy/Astronomy/Views/LSIImageCollectionViewCell.h
@@ -0,0 +1,17 @@
+//
+//  LSIImageCollectionViewCell.h
+//  Astronomy
+//
+//  Created by David Wright on 6/3/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIImageCollectionViewCell : UICollectionViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
