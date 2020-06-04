b642c2e7e61cf82b0aef99097d11578ca36904ca
David Wright <davidtwright89@gmail.com>
2020-06-04 00:50:09 -0400

    Implemented the Swift model: MarsPhotoReference

--- Astronomy/Astronomy/Models/MarsPhoto.swift
+++ Astronomy/Astronomy/Models/MarsPhoto.swift
@@ -9,5 +9,25 @@
 import Foundation
 
 class MarsPhotoReference {
+    let id: Int
+    let sol: Int
+    let camera: Camera
+    let earthDate: Date
+    let imageURL: URL
     
+    static let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.timeZone = TimeZone(abbreviation: "GMT")
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter
+    }()
+    
+    init(id: Int, sol: Int, camera: Camera, earthDate: Date, imageURL: URL) {
+        self.id = id
+        self.sol = sol
+        self.camera = camera
+        self.earthDate = earthDate
+        self.imageURL = imageURL
+    }
 }
