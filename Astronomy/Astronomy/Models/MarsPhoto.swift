5ca02b6ebae1708bb8573a1456e8feb31566c3b8
David Wright <davidtwright89@gmail.com>
2020-06-03 23:20:39 -0400

    Added a Swift model class: MarsPhotoReference

---
+++ Astronomy/Astronomy/Models/MarsPhoto.swift
@@ -0,0 +1,13 @@
+//
+//  MarsPhoto.swift
+//  Astronomy
+//
+//  Created by David Wright on 6/3/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+import Foundation
+
+class MarsPhotoReference {
+    
+}
