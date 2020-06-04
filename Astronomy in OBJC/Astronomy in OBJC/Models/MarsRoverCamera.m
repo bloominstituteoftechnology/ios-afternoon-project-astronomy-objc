31755e2bb2d559092d906f4683868cf2a182275e
Sal <salmanamer@gmail.com>
2020-06-04 03:34:15 -0700

    added and initialed MarsRoverCamera model in OBJC

--- Astronomy in OBJC/Astronomy in OBJC/Models/MarsRoverCamera.m
+++ Astronomy in OBJC/Astronomy in OBJC/Models/MarsRoverCamera.m
@@ -10,4 +10,19 @@
 
 @implementation MarsRoverCamera
 
+-(instancetype) initWithIdentity:(NSNumber *)identity
+                            name:(NSString *)name
+                         roverId:(NSNumber *)roverId
+                        fullName:(NSString *)fullName
+{
+  self = [super init];
+  if (self) {
+    _identity = identity;
+    _name = name;
+    _roverId = roverId;
+    fullName = fullName;
+  }
+  return self;
+}
+
 @end
