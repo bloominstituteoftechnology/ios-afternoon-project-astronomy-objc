59917fd3f5b46f94a78d4cd1d503b11be15990a8
Sal <salmanamer@gmail.com>
2020-06-04 02:39:58 -0700

    renamed project.. added - to all spaces

---
+++ Astronomy in OBJC/Astronomy in OBJC/Views/RoverCollectionViewCell.h
@@ -0,0 +1,17 @@
+//
+//  RoverCollectionViewCell.h
+//  Astronomy in OBJC
+//
+//  Created by Sal B Amer on 6/4/20.
+//  Copyright © 2020 Sal B Amer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RoverCollectionViewCell : UICollectionViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Astronomy in OBJC/Astronomy in OBJC/Views/RoverCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  RoverCollectionViewCell.m
+//  Astronomy in OBJC
+//
+//  Created by Sal B Amer on 6/4/20.
+//  Copyright © 2020 Sal B Amer. All rights reserved.
+//
+
+#import "RoverCollectionViewCell.h"
+
+@implementation RoverCollectionViewCell
+
+@end
