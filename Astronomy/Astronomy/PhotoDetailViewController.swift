29f0b66c531e8742ee11011a6f3b1ab8088760d2
Jessie <jessie.griffin@icloud.com>
2020-06-03 22:37:00 -0400

    Adds CollectionViewController and DetailViewController and Bridging Header

---
+++ Astronomy/Astronomy/PhotoDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoDetailViewController.swift
+//  Astronomy
+//
+//  Created by Jessie Ann Griffin on 6/3/20.
+//  Copyright © 2020 Jessie Griffin. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
