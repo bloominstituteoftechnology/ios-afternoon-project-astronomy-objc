767adbd64b7710bc304ec9e2340c179404002b9d
Jessie <jessie.griffin@icloud.com>
2020-06-03 23:33:46 -0400

    Adds implementation for MarsPhoto initWithID

--- Astronomy/Astronomy/CATMarsPhoto.m
+++ Astronomy/Astronomy/CATMarsPhoto.m
@@ -10,4 +10,25 @@
 
 @implementation CATMarsPhoto
 
+- (instancetype)initWithID:(int)anIdentifier
+                       sol:(int)aSol
+                    camera:(CATCamera *)aCamera
+                 earthDate:(NSDate *)earthDate
+                  imageURL:(NSURL *)imageURL
+{
+    if (self = [super init]) {
+        _identifier = anIdentifier;
+        _sol = aSol;
+        _camera = aCamera;
+        _earthDate = earthDate;
+        _imageURL = imageURL;
+    }
+    return self;
+}
+
+//- (nullable instancetype)initWithDictionary:(NSDictionary *)dictionary
+//{
+//    
+//}
+
 @end
