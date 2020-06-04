399b259339ab65c774f69c2ed685a2202d74768b
David Wright <davidtwright89@gmail.com>
2020-06-04 00:39:58 -0400

    Manually created a bridging header since Xcode did not ask to generate one for me. Entered the bridging header file path in the build settings

---
+++ Astronomy/Astronomy/Resources/Astronomy-Bridging-Header.h
@@ -0,0 +1,19 @@
+//
+//  Astronomy-Bridging-Header.h
+//  Astronomy
+//
+//  Created by David Wright on 6/4/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#ifndef Astronomy_Bridging_Header_h
+#define Astronomy_Bridging_Header_h
+
+
+#endif /* Astronomy_Bridging_Header_h */
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+
