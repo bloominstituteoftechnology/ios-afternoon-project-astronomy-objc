0230f6c87cee7945755a43a2df2716a2106d5523
Sal <salmanamer@gmail.com>
2020-06-04 23:26:07 -0700

    fixed spacing issues between collection view cells

--- Astronomy in OBJC/Astronomy in OBJC/Resources/Astronomy_in_OBJC-Bridging-Header.h
+++ Astronomy in OBJC/Astronomy in OBJC/Resources/Astronomy_in_OBJC-Bridging-Header.h
@@ -10,7 +10,7 @@
 
 #ifndef Astronomy_in_OBJC_Bridging_Header_h
 #define Astronomy_in_OBJC_Bridging_Header_h
-#import "/Users/lambdacatalina/Desktop/LAMBDA-SCHOOL/Unit 4 - Objc Swift Combo - Astronomy HW/ios-afternoon-project-astronomy-objc/Astronomy in OBJC/Astronomy in OBJC/Model Controllers/RoverAPIController.h"
+#import "../Model Controllers/RoverAPIController.h"
 #import "../Views/RoverCollectionViewCell.h"
 #import "MarsPhotos.h"
 #import "SolDescription.h"
