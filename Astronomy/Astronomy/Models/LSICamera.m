c0c575629520191287f66546c27e12209cc922c8
David Wright <davidtwright89@gmail.com>
2020-06-06 18:31:54 -0400

    Implemented LSICamera

--- Astronomy/Astronomy/Models/LSICamera.m
+++ Astronomy/Astronomy/Models/LSICamera.m
@@ -10,4 +10,35 @@
 
 @implementation LSICamera
 
+- (instancetype)initWithCameraId:(int)cameraId roverId:(int)roverId name:(NSString *)name fullName:(NSString *)fullName
+{
+    if (self = [super init]) {
+        _cameraId = cameraId;
+        _roverId = roverId;
+        _name = name.copy;
+        _fullName = fullName.copy;
+    }
+    return self;
+}
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary
+{
+    NSNumber *cameraId = [dictionary objectForKey:@"id"];
+    if (![cameraId isKindOfClass:[NSNumber class]]) return nil;
+    
+    NSNumber *roverId = [dictionary objectForKey:@"rover_id"];
+    if (![roverId isKindOfClass:[NSNumber class]]) return nil;
+    
+    NSString *name = [dictionary objectForKey:@"name"];
+    if (![name isKindOfClass:[NSString class]]) return nil;
+    
+    NSString *fullName = [dictionary objectForKey:@"full_name"];
+    if (![name isKindOfClass:[NSString class]]) return nil;
+    
+    return [self initWithCameraId:cameraId.intValue
+                          roverId:roverId.intValue
+                             name:name
+                         fullName:fullName];
+}
+
 @end
