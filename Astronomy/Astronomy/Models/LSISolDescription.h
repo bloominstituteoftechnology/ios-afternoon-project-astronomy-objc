9a85e5c2a5780b4a70b51cd1d63d1e505fbc6e5b
David Wright <davidtwright89@gmail.com>
2020-06-06 18:27:57 -0400

    Declared the public interface for LSISolDescription

--- Astronomy/Astronomy/Models/LSISolDescription.h
+++ Astronomy/Astronomy/Models/LSISolDescription.h
@@ -8,10 +8,22 @@
 
 #import <Foundation/Foundation.h>
 
-NS_ASSUME_NONNULL_BEGIN
-
+NS_SWIFT_NAME(SolDescription)
 @interface LSISolDescription : NSObject
 
-@end
+@property (nonatomic, readonly) int sol;
+@property (nonatomic, readonly) int totalPhotos;
+@property (nonatomic, copy, readonly, nonnull) NSArray<NSString *> *cameras;
 
-NS_ASSUME_NONNULL_END
+// Unused API values
+/*
+@property (nonatomic, readonly, nonnull) NSDate *earthDate;
+*/
+
+- (nonnull instancetype)initWithSol:(int)sol
+                        totalPhotos:(int)totalPhotos
+                            cameras:(nonnull NSArray<NSString *> *)cameras;
+
+- (nullable instancetype)initWithDictionary:(nonnull NSDictionary *)dictionary;
+
+@end
