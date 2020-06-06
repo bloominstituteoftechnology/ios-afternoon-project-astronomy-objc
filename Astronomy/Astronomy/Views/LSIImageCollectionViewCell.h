195c8d339df3dafd05c5a2e866fb1311a8ad5513
David Wright <davidtwright89@gmail.com>
2020-06-06 19:58:55 -0400

    Declared the public interface for LSIImageCollectionViewCell

--- Astronomy/Astronomy/Views/LSIImageCollectionViewCell.h
+++ Astronomy/Astronomy/Views/LSIImageCollectionViewCell.h
@@ -8,10 +8,10 @@
 
 #import <UIKit/UIKit.h>
 
-NS_ASSUME_NONNULL_BEGIN
-
+NS_SWIFT_NAME(ImageCollectionViewCell)
 @interface LSIImageCollectionViewCell : UICollectionViewCell
+
+@property (nonatomic, readwrite, nullable) UIImage *image;
 
 @end
 
-NS_ASSUME_NONNULL_END
