8aee8325a2aad991e23caa2592000ae1b9dc24c3
Jessie <jessie.griffin@icloud.com>
2020-06-05 22:03:23 -0400

    Adds method stubs and url "computed properties" to MarsRoverClient

--- Astronomy/Astronomy/CATMarsRoverClient.h
+++ Astronomy/Astronomy/CATMarsRoverClient.h
@@ -7,10 +7,24 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "CATMarsPhoto.h"
+#import "Astronomy-Swift.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
+typedef void (^MarsRoverCompletion)(CATMarsRover *rover, NSError *error);
+typedef void (^MarsPhotosCompletion)(NSArray <CATMarsPhoto *> *photos, NSError *error);
+
+NS_SWIFT_NAME(MarsRoverClient)
 @interface CATMarsRoverClient : NSObject
+
+- (void)fetchMarsRoverWithName:(NSString *)name usingSession:(NSURLSession *)session completion:(MarsRoverCompletion)completion;
+
+- (void)saveMarsRoverToLocal:(MarsRoverCompletion)completion;
+
+- (void)fetchPhotosFrom:(CATMarsRover *)rover onSol:(int)sol usingSession:(NSURLSession *)session completion:(MarsPhotosCompletion)photos;
+
+- (void)fetchLocalPhotosFrom:(CATMarsRover *)rover onSol:(int)sol completion:(MarsPhotosCompletion)photos;
 
 @end
 
--- Astronomy/Astronomy/CATMarsRoverClient.m
+++ Astronomy/Astronomy/CATMarsRoverClient.m
@@ -8,6 +8,61 @@
 
 #import "CATMarsRoverClient.h"
 
+@interface CATMarsRoverClient ()
+
+
+
+@end
+
 @implementation CATMarsRoverClient
+
+- (void)fetchMarsRoverWithName:(NSString *)name usingSession:(NSURLSession *)session completion:(MarsRoverCompletion)completion
+{
+
+}
+
+- (void)saveMarsRoverToLocal:(MarsRoverCompletion)completion
+{
+
+}
+
+- (void)fetchPhotosFrom:(CATMarsRover *)rover onSol:(int)sol usingSession:(NSURLSession *)session completion:(MarsPhotosCompletion)photos
+{
+
+}
+
+- (void)fetchLocalPhotosFrom:(CATMarsRover *)rover onSol:(int)sol completion:(MarsPhotosCompletion)photos
+{
+
+}
+
+- (NSURL *)URLforInfoForRover:(NSString *)roverName
+{
+    NSURL *baseURL = [NSURL URLWithString:@"https://api.nasa.gov/mars-photos/api/v1"];
+    NSString *apiKey = @"dbK4pfi2Sb18KBfNPjlhYTeIcd5GyVIzwqzIAa5K";
+    [baseURL URLByAppendingPathComponent:@"manifests"];
+    [baseURL URLByAppendingPathComponent:roverName];
+    NSURLComponents *urlComponents = [NSURLComponents componentsWithURL:baseURL resolvingAgainstBaseURL:YES];
+    urlComponents.queryItems = @[[NSURLQueryItem queryItemWithName:@"api_key" value:apiKey]];
+    NSURL *url = urlComponents.URL;
+    return url;
+}
+
+- (NSURL *)URLforPhotosFromRover:(NSString *)roverName onSol:(int)sol
+{
+    NSURL *baseURL = [NSURL URLWithString:@"https://api.nasa.gov/mars-photos/api/v1"];
+    NSString *apiKey = @"dbK4pfi2Sb18KBfNPjlhYTeIcd5GyVIzwqzIAa5K";
+    [baseURL URLByAppendingPathComponent:@"rovers"];
+    [baseURL URLByAppendingPathComponent:roverName];
+    [baseURL URLByAppendingPathComponent:@"photos"];
+    NSURLComponents *urlComponents = [NSURLComponents componentsWithURL:baseURL resolvingAgainstBaseURL:YES];
+    urlComponents.queryItems = @[
+        [NSURLQueryItem queryItemWithName:@"sol" value:[NSString stringWithFormat:@"%d", sol]],
+        [NSURLQueryItem queryItemWithName:@"api_key" value:apiKey],
+    ];
+    NSURL *url = urlComponents.URL;
+    return url;
+}
+
 
 @end
