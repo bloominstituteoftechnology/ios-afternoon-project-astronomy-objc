e37e9a723b790bb42065029fae8a6ebb93031e64
David Wright <davidtwright89@gmail.com>
2020-06-06 18:26:24 -0400

    Implemented LSISolDescription

--- Astronomy/Astronomy/Models/LSIMarsRover.m
+++ Astronomy/Astronomy/Models/LSIMarsRover.m
@@ -10,4 +10,38 @@
 
 @implementation LSIMarsRover
 
+- (instancetype)initWithName:(NSString *)name
+              numberOfPhotos:(int)numberOfPhotos
+                      maxSol:(int)maxSol
+             solDescriptions:(NSArray<NSDictionary *> *)solDescriptions
+{
+    if (self = [super init]) {
+        _name = name.copy;
+        _numberOfPhotos = numberOfPhotos;
+        _maxSol = maxSol;
+        _solDescriptions = solDescriptions.copy;
+    }
+    return self;
+}
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary
+{
+    NSString *name = [dictionary objectForKey:@"name"];
+    if (![name isKindOfClass:[NSString class]]) return nil;
+    
+    NSNumber *numberOfPhotos = [dictionary objectForKey:@"total_photos"];
+    if (![numberOfPhotos isKindOfClass:[NSNumber class]]) return nil;
+    
+    NSNumber *maxSol = [dictionary objectForKey:@"max_sol"];
+    if (![maxSol isKindOfClass:[NSNumber class]]) return nil;
+    
+    NSArray *solDescriptions = [dictionary objectForKey:@"photos"];
+    if (![solDescriptions isKindOfClass:[NSArray class]]) return nil;
+    
+    return [self initWithName:name
+               numberOfPhotos:numberOfPhotos.intValue
+                       maxSol:maxSol.intValue
+              solDescriptions:solDescriptions];
+}
+
 @end
