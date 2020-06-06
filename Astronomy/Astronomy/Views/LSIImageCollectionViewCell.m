bae71fc3c746df51eb4783f4fb2f3e0dd1a0f77c
David Wright <davidtwright89@gmail.com>
2020-06-06 19:59:27 -0400

    Implemented LSIImageCollectionViewCell

--- Astronomy/Astronomy/Views/LSIImageCollectionViewCell.m
+++ Astronomy/Astronomy/Views/LSIImageCollectionViewCell.m
@@ -8,6 +8,31 @@
 
 #import "LSIImageCollectionViewCell.h"
 
+@interface LSIImageCollectionViewCell ()
+
+@property (nonatomic, readwrite, nonnull) IBOutlet UIImageView *imageView;
+
+@end
+
 @implementation LSIImageCollectionViewCell
+
+// Custom getter/setter for image
+@synthesize image = _image;
+
+- (UIImage *)image
+{
+    return _image;
+}
+
+- (void)setPhoto:(UIImage * _Nonnull)image
+{
+    _image = image;
+    
+    if (image) {
+        self.imageView.image = image;
+    } else {
+        self.imageView.image = [UIImage imageNamed:@"MarsPlaceholder"];
+    }
+}
 
 @end
