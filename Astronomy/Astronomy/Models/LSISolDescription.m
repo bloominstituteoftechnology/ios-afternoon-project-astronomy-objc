5e1538b4f2ceb72ad452ec892b65b5414179fa23
David Wright <davidtwright89@gmail.com>
2020-06-06 18:28:24 -0400

    Implemented LSISolDescription

--- Astronomy/Astronomy/Models/LSISolDescription.m
+++ Astronomy/Astronomy/Models/LSISolDescription.m
@@ -10,4 +10,32 @@
 
 @implementation LSISolDescription
 
+- (instancetype)initWithSol:(int)sol
+                totalPhotos:(int)totalPhotos
+                    cameras:(NSArray<NSString *> *)cameras
+{
+    if (self = [super init]) {
+        _sol = sol;
+        _totalPhotos = totalPhotos;
+        _cameras = cameras.copy;
+    }
+    return self;
+}
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary
+{
+    NSNumber *sol = [dictionary objectForKey:@"sol"];
+    if (![sol isKindOfClass:[NSNumber class]]) return nil;
+    
+    NSNumber *totalPhotos = [dictionary objectForKey:@"total_photos"];
+    if (![totalPhotos isKindOfClass:[NSNumber class]]) return nil;
+    
+    NSArray *cameras = [dictionary objectForKey:@"cameras"];
+    if (![cameras isKindOfClass:[NSArray class]]) return nil;
+    
+    return [self initWithSol:sol.intValue
+                 totalPhotos:totalPhotos.intValue
+                     cameras:cameras];
+}
+
 @end
