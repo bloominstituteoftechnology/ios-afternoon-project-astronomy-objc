555227bbf9009426518e9b45fbe54b95a1dc89cc
David Wright <davidtwright89@gmail.com>
2020-06-07 08:38:10 -0400

    Added a LSIFetchPhotoOperation class

---
+++ Astronomy/Astronomy/Operations/LSIFetchPhotoOperation.h
@@ -0,0 +1,17 @@
+//
+//  LSIFetchPhotoOperation.h
+//  Astronomy
+//
+//  Created by David Wright on 6/7/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIFetchPhotoOperation : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Astronomy/Astronomy/Operations/LSIFetchPhotoOperation.m
@@ -0,0 +1,13 @@
+//
+//  LSIFetchPhotoOperation.m
+//  Astronomy
+//
+//  Created by David Wright on 6/7/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import "LSIFetchPhotoOperation.h"
+
+@implementation LSIFetchPhotoOperation
+
+@end
