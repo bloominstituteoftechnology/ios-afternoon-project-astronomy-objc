a50f79da4ec8a04d6fedac45206b309ad5b24cc0
David Wright <davidtwright89@gmail.com>
2020-06-06 20:02:39 -0400

    Implemented PhotoDetailViewController

--- Astronomy/Astronomy/View Controllers/PhotoDetailViewController.swift
+++ Astronomy/Astronomy/View Controllers/PhotoDetailViewController.swift
@@ -7,24 +7,73 @@
 //
 
 import UIKit
+import Photos
 
 class PhotoDetailViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    // MARK: - Properties & Outlets
+    
+    var photo: MarsPhotoReference? {
+        didSet {
+            updateViews()
+        }
     }
     
+    lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .short
+        return formatter
+    }()
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var detailLabel: UILabel!
+    @IBOutlet var cameraLabel: UILabel!
+    
+    
+    // MARK: - View Controller Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateViews()
     }
-    */
-
+    
+    // MARK: - IBActions & Methods
+    
+    @IBAction func save(_ sender: Any) {
+        guard let image = imageView.image else { return }
+        PHPhotoLibrary.shared().performChanges({
+            PHAssetChangeRequest.creationRequestForAsset(from: image)
+        }, completionHandler: { (success, error) in
+            if let error = error {
+                NSLog("Error saving photo: \(error)")
+                return
+            }
+            DispatchQueue.main.async {
+                self.presentSuccessfulSaveAlert()
+            }
+        })
+    }
+    
+    private func updateViews() {
+        guard let photo = photo, isViewLoaded else { return }
+        do {
+            let data = try Data(contentsOf: photo.imageURL.usingHTTPS!)
+            imageView.image = UIImage(data: data)
+            let dateString = dateFormatter.string(from: photo.earthDate)
+            detailLabel.text = "Taken by \(photo.camera.roverId) on \(dateString) (Sol \(photo.sol))"
+            cameraLabel.text = photo.camera.fullName
+        } catch {
+            NSLog("Error setting up views on detail view controller: \(error)")
+        }
+    }
+    
+    private func presentSuccessfulSaveAlert() {
+        let alert = UIAlertController(title: "Photo Saved!", message: "The photo has been saved to your Photo Library!", preferredStyle: .alert)
+        let okayAction = UIAlertAction(title: "Okay", style: .default, handler: nil)
+        
+        alert.addAction(okayAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
 }
