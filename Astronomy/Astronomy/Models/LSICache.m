73b59b0213ef5a655cd33ac8b081072a077447f1
David Wright <davidtwright89@gmail.com>
2020-06-10 01:52:04 -0400

    Added a custom LSICache class

---
+++ Astronomy/Astronomy/Models/LSICache.m
@@ -0,0 +1,13 @@
+//
+//  LSICache.m
+//  Astronomy
+//
+//  Created by David Wright on 6/10/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import "LSICache.h"
+
+@implementation LSICache
+
+@end
