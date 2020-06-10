eac1c047feef606b29f24cdfbba220664d6b60fd
David Wright <davidtwright89@gmail.com>
2020-06-10 03:30:11 -0400

    Implemented LSICache

--- Astronomy/Astronomy/Models/LSICache.m
+++ Astronomy/Astronomy/Models/LSICache.m
@@ -8,6 +8,45 @@
 
 #import "LSICache.h"
 
+@interface LSICache ()
+
+@property (copy, readwrite) NSMutableDictionary<NSNumber *, id> *cache;
+@property (copy, readonly) dispatch_queue_t queue;
+
+@end
+
+
 @implementation LSICache
+
+- (instancetype)init
+{
+    if (self = [super init]) {
+        _queue = dispatch_queue_create([@"AstronomyCacheQueue" UTF8String], nil);
+    }
+    return self;
+}
+
+- (void)cacheValue:(id)value ForKey:(NSNumber *)key
+{
+    dispatch_async(self.queue, ^{
+        [self.cache setObject:value forKey:key];
+    });
+}
+
+- (id)valueForKey:(NSNumber *)key
+{
+    __block id value;
+    dispatch_sync(self.queue, ^{
+        value = [self.cache objectForKey:key];
+    });
+    return value;
+}
+
+- (void)clear
+{
+    dispatch_async(self.queue, ^{
+        [self.cache removeAllObjects];
+    });
+}
 
 @end
