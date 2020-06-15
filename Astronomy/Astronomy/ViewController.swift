09e9c669c4629930a2a9df402552579b084f603e
Chris Dobek <dobek.christopher@gmail.com>
2020-06-15 16:25:59 -0400

    Created New Project and added LambdaSDK file to project

---
+++ Astronomy/Astronomy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Astronomy
+//
+//  Created by Chris Dobek on 6/15/20.
+//  Copyright © 2020 Chris Dobek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
