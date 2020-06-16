ace0be68ce9ba22b45c13ce5e3687f666a099659
Patrick Millet <lohs79seahawk@gmail.com>
2020-06-16 16:11:19 -0500

    Created project, relocated plist and changed build setting, created groups to store MVC. Added a camera model to handle the assignment to object from API.

---
+++ Obj-C-Astronomy/Obj-C-Astronomy/Model/Camera.h
@@ -0,0 +1,27 @@
+//
+//  Camera.h
+//  Obj-C-Astronomy
+//
+//  Created by Patrick Millet on 6/16/20.
+//  Copyright © 2020 PatrickMillet79. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Camera : NSObject
+
+@property (nonatomic, readonly) int identifier; // named "id" in api
+@property (nonatomic, readonly, copy) NSString *name;
+@property (nonatomic, readonly) int roverID; // named "rover_id" in api
+@property (nonatomic, readonly, copy) NSString *fullName;
+
+- (instancetype)initWithIdentifier:(int)identifier
+                              name:(NSString *)name
+                           roverID:(int)roverID
+                          fullName:(NSString *)fullName;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Obj-C-Astronomy/Obj-C-Astronomy/Model/Camera.m
@@ -0,0 +1,27 @@
+//
+//  Camera.m
+//  Obj-C-Astronomy
+//
+//  Created by Patrick Millet on 6/16/20.
+//  Copyright © 2020 PatrickMillet79. All rights reserved.
+//
+
+#import "Camera.h"
+
+@implementation Camera
+
+- (instancetype)initWithIdentifier:(int)identifier
+                              name:(NSString *)name
+                           roverID:(int)roverID
+                          fullName:(NSString *)fullName
+{
+    if (self = [super init]) {
+        _identifier = identifier;
+        _name = name;
+        _roverID = roverID;
+        _fullName = fullName;
+    }
+    return self;
+}
+
+@end
