c6abf462dc7c38e12fcf65d31164ad26c1c3c2e2
Chris Dobek <dobek.christopher@gmail.com>
2020-06-17 15:17:46 -0400

    Files for storyboard created and connected

---
+++ Astronomy/Astronomy/View Controllers/CMDPhotoCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  CMDPhotoCollectionViewCell.m
+//  Astronomy
+//
+//  Created by Chris Dobek on 6/17/20.
+//  Copyright © 2020 Chris Dobek. All rights reserved.
+//
+
+#import "CMDPhotoCollectionViewCell.h"
+
+@implementation CMDPhotoCollectionViewCell
+
+@end
