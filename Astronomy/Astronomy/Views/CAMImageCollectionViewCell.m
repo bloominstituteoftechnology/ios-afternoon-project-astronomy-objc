b50ced42281ce82d3187240dcbcf1ff545476432
Cody Morley <codymorleyinc@gmail.com>
2020-07-20 14:45:44 -0700

    Main.storyboard, ViewControllers, Collection View Cell
    Finished importing UI from original project.

---
+++ Astronomy/Astronomy/Views/CAMImageCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  CAMImageCollectionViewCell.m
+//  Astronomy
+//
+//  Created by Cody Morley on 7/20/20.
+//  Copyright © 2020 Cody Morley. All rights reserved.
+//
+
+#import "CAMImageCollectionViewCell.h"
+
+@implementation CAMImageCollectionViewCell
+
+@end
