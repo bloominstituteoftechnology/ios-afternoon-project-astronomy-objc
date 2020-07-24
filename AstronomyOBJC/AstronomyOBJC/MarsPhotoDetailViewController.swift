661dd37fcfa90f97d668c35b5f34731880fe5a2c
Kelson Hartle <kelson99hartle@icloud.com>
2020-07-23 20:28:38 -0600

    added stubs for collection view and photo detail view, as well as created basic outline for storyboard

---
+++ AstronomyOBJC/AstronomyOBJC/MarsPhotoDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MarsPhotoDetailViewController.swift
+//  AstronomyOBJC
+//
+//  Created by Kelson Hartle on 7/23/20.
+//  Copyright © 2020 Kelson Hartle. All rights reserved.
+//
+
+import UIKit
+
+class MarsPhotoDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
