59992ef9dbfe67bcbd96de4fb88ea7a3f5d54883
Kelson Hartle <kelson99hartle@icloud.com>
2020-07-23 20:14:17 -0600

    added project and implemented lambda sdk files in project

---
+++ AstronomyOBJC/AstronomyOBJC/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  AstronomyOBJC
+//
+//  Created by Kelson Hartle on 7/23/20.
+//  Copyright © 2020 Kelson Hartle. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
