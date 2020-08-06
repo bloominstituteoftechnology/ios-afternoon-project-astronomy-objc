18ec862543b5b8fe0f8830747c18873787ed5603
Chad Parker <chad@chadparker.com>
2020-08-05 20:49:42 -0700

    Add PhotoDetailVC

---
+++ Astronomy-ObjC/View Controllers/PhotoDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoDetailViewController.swift
+//  Astronomy-ObjC
+//
+//  Created by Chad Parker on 8/5/20.
+//  Copyright © 2020 Chad Parker. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
