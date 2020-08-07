76bf63c93046fd682481cc28b0b9be9908a52cdc
Claudia Maciel <clc_80@yahoo.com>
2020-08-06 17:24:48 -0700

    Added Photo Class

---
+++ Astronomy/Astronomy/CLMPhoto.swift
@@ -0,0 +1,13 @@
+//
+//  CLMPhoto.swift
+//  Astronomy
+//
+//  Created by Claudia Maciel on 8/6/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+import UIKit
+
+class CLMPhoto: NSObject {
+
+}
