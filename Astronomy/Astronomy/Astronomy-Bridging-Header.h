1f615fcf58b1fe33adc135be22148a7eb7c3c0cc
Claudia Maciel <clc_80@yahoo.com>
2020-08-06 18:14:34 -0700

    Added bridging file

---
+++ Astronomy/Astronomy/Astronomy-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Astronomy-Bridging-Header.h
+//  Astronomy
+//
+//  Created by Claudia Maciel on 8/6/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+#ifndef Astronomy_Bridging_Header_h
+#define Astronomy_Bridging_Header_h
+
+
+#endif /* Astronomy_Bridging_Header_h */
