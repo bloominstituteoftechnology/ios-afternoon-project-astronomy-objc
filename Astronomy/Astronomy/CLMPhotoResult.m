e82ae0fa22c4530596812e710fdaf48818c6ad98
Claudia Maciel <clc_80@yahoo.com>
2020-08-06 17:33:56 -0700

    Created photoResult class

---
+++ Astronomy/Astronomy/CLMPhotoResult.m
@@ -0,0 +1,13 @@
+//
+//  CLMPhotoResult.m
+//  Astronomy
+//
+//  Created by Claudia Maciel on 8/6/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+#import "CLMPhotoResult.h"
+
+@implementation CLMPhotoResult
+
+@end
