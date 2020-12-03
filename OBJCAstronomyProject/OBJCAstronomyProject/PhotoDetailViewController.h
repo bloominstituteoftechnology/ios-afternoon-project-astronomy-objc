74dad67895724c962d01c1e2378db0de1c912ede
brys <brys1saclaus2@gmail.com>
2020-12-02 19:47:02 -0800

    Stroyboard set up. added viewController files

---
+++ OBJCAstronomyProject/OBJCAstronomyProject/PhotoDetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  PhotoDetailViewController.h
+//  OBJCAstronomyProject
+//
+//  Created by BrysonSaclausa on 12/2/20.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PhotoDetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ OBJCAstronomyProject/OBJCAstronomyProject/PhotosCollectionViewController.h
@@ -0,0 +1,16 @@
+//
+//  PhotosCollectionViewController.h
+//  OBJCAstronomyProject
+//
+//  Created by BrysonSaclausa on 12/2/20.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PhotosCollectionViewController : UICollectionViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
