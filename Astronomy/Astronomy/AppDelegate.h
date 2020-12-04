837ef75a24951c45bf8506c4c88bdf794c38d62d
Cora Jacobson <cora-jacobson@lambdastudents.com>
2020-12-03 18:06:18 -0800

    Set up project

---
+++ Astronomy/Astronomy/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Astronomy
+//
+//  Created by Cora Jacobson on 12/3/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
