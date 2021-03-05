00d24ae693b3a163d4bf04353141aa016078546c
James McDougall <jamesmcdougall@James-MBP.lan>
2021-03-05 13:12:34 -0600

    Initial Project Commit

---
+++ Astronomy - ObjC/Astronomy - ObjC/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Astronomy - ObjC
+//
+//  Created by James McDougall on 3/5/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Astronomy - ObjC/Astronomy - ObjC/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Astronomy - ObjC
+//
+//  Created by James McDougall on 3/5/21.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
